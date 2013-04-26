40b46e0f2fe4f48671a3072ae5e63aecffa96187
pks90 <prateek17jan@gmail.com>
2013-04-26 15:11:34 +0530

    Initial Commit

---
+++ GitUSeExample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitUSeExample
+//
+//  Created by Binary  Semantics on 4/26/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GitUSeExample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GitUSeExample
+//
+//  Created by Binary  Semantics on 4/26/13.
+//  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
